9deffc3f9a0baeb01bddac617adf1bcb69bcd3ff
Beth Tenorio <betsky98@gmail.com>
2013-10-04 14:06:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Irdb3::Application.config.secret_key_base = 'f1802bc2562abcef519914ce9e0a89b0ac353bd92c30b670ec9c60c1abfbbf04adf78378f7be56771c34f957bfeaee1dd9006071a54951f7930772404bb6f6f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Irdb3::Application.config.session_store :cookie_store, key: '_irdb3_session'
