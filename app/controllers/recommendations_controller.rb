2f60d796e9acb1270ea4f746f56e4318a2d10823
Beth Tenorio <betsky98@gmail.com>
2013-10-16 15:34:35 -0400

    Fix logo and navigation links

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -5,9 +5,10 @@ class RecommendationsController < ApplicationController
     recommendation.user_id = current_user.id
     recommendation.posting_id = posting.id
     if recommendation.save
-      redirect_to(postings_path, notice: "You recommended #{posting.title}")
+      redirect_to(:back, notice: "You recommended #{posting.title}")
     else
-      redirect_to(postings_path, notice: "You already recommended this")
+      redirect_to(:back, notice: "You already recommended #{posting.title}")
+
     end
   end
 
